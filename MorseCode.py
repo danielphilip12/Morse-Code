e310bd384fe8cc461b9ec2cc3846f7ffbdecf572
danielphilip12 <35010525+danielphilip12@users.noreply.github.com>
2018-10-26 08:30:54 -0700

    Add Morse code translator

--- MorseCode.py
+++ MorseCode.py
@@ -26,6 +26,9 @@ morse['x'] = "-..-"
 morse['y'] = "-.--"
 morse['z'] = "--.."
 morse[" "] = "/"
+reverse_morse = {}
+for x,y in zip(morse.keys(), morse.values()):
+    reverse_morse[y] = x
 def convert_word(sent, dict=morse):
     newSent = ""
     sent = sent.lower()
@@ -36,6 +39,19 @@ def convert_word(sent, dict=morse):
             newSent += i
 
     print(newSent)
+def convert_morse(sent, dict=reverse_morse):
+    newSent = ""
+    sent = sent.split(" ")
+    for i in sent:
+        if i in dict:
+            newSent += dict[i]
+        else:
+            newSent += " "
+
+    print(newSent)
 
 sentence = input("Enter a sentence: ")
 convert_word(sentence)
+
+sent = input("Enter morse code:")
+convert_morse(sent)
